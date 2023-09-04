841ed617e98eeec84736bb5fce2b189ccc06255b
NicholasBaldwinSE <nicholasbaldwinse@proton.me>
2023-09-04 16:36:37 -0400

    Finalize basic in-memory dictionary-based prototype.

--- main.py
+++ main.py
@@ -26,12 +26,16 @@ POINT_REGEX = re.compile(r"(\+|-)?(\d+)")
 
 # Simple point accumulation/decumulation.
 @bot.command()
-async def point(ctx: commands.Context, modifier: str, user: discord.Member):
+async def point(ctx: commands.Context, modifier: str, user: discord.Member, name: str):
     if ctx.guild.id not in bot.points_cache:
         bot.points_cache[ctx.guild.id] = {}
     
-    if user.id not in bot.points_cache[ctx.guild.id]:
-        bot.points_cache[ctx.guild.id][user.id] = 0
+    if name not in bot.points_cache[ctx.guild.id]:
+        await ctx.send(f"Point type {name} does not exist.")
+        return
+
+    if user.id not in bot.points_cache[ctx.guild.id][name]:
+        bot.points_cache[ctx.guild.id][name][user.id] = 0
 
     if (match := POINT_REGEX.match(modifier)):
         state = match.group(1)
@@ -41,9 +45,62 @@ async def point(ctx: commands.Context, modifier: str, user: discord.Member):
         if state == "-":
             num *= -1
     
-        bot.points_cache[ctx.guild.id][user.id] += num
+        bot.points_cache[ctx.guild.id][name][user.id] += num
 
-    await ctx.send(f"User {user.name} has {bot.points_cache[ctx.guild.id][user.id]} points.")
+    await ctx.send(f"User {user.name} has {bot.points_cache[ctx.guild.id][name][user.id]} {name} points.")
+
+@bot.command()
+async def addpoint(ctx: commands.Context, name: str):
+    # Adds a type of point to the server's list.
+    if ctx.guild.id not in bot.points_cache:
+        bot.points_cache[ctx.guild.id] = {}
+    
+    # Limit each server to 3 different kinds of points.
+    if len(bot.points_cache[ctx.guild.id]) == 3:
+        await ctx.send("Reached cap of 3 different types of points. Either rename or delete a type.")
+        return
+
+    bot.points_cache[ctx.guild.id][name] = {}
+
+    await ctx.send(f"Created point type {name}.")
+
+@bot.command()
+async def delpoint(ctx: commands.Context, name: str):
+    # Removes a type of point from the server's list.
+    if ctx.guild.id not in bot.points_cache:
+        return
+
+    # Check if no point types exist.
+    if len(bot.points_cache[ctx.guild.id]) == 0:
+        await ctx.send("Cannot remove point type, no point types exist.")
+        return
+
+    # Check if specific point type doesn't exist.
+    if name not in bot.points_cache[ctx.guild.id]:
+        await ctx.send(f"Cannot remove point type {name}, does not exist.")
+        return
+
+    del bot.points_cache[ctx.guild.id][name]
+
+    await ctx.send(f"Deleted point type {name}.")
+
+@bot.command()
+async def renamepoint(ctx: commands.Context, name: str, newname: str):
+    # Renames a type of point.
+    if ctx.guild.id not in bot.points_cache:
+        return
+
+    if name not in bot.points_cache[ctx.guild.id]:
+        await ctx.send(f"Cannot rename point type {name}. Does not exist.")
+        return
+
+    # This effectively renames the point. Note, it is not very
+    # efficient and probably won't translate well to Redis/SQL.
+    # Will need a better system for storing point names such as 
+    # an internal ID system.
+    bot.points_cache[ctx.guild.id][newname] = bot.points_cache[ctx.guild.id].pop(name)
+
+    await ctx.send(f"Renamed point type {name} to {newname}.")
 
 # Run bot using token.
 bot.run(os.getenv("TOKEN"))
